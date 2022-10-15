ebe9b1758ca3b4322931963409a902d0279a02da
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-15 13:16:34 -0400

    JS updates, seems working but needs loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// sets the date format for the header section currentDay id for showing on page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 $(document).ready(function () {
@@ -10,9 +11,52 @@ $(document).ready(function () {
         // Save time as key and text as value in local storage
         localStorage.setItem(time, text);
         })
-
+    
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
         
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id"));
+        
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    
+    // store values on click
+    // need to figure out how to loop this process, below works but not good code
+    $("#8.description").val(localStorage.getItem("8"));
+    $("#9.description").val(localStorage.getItem("9"));
+    $("#10.description").val(localStorage.getItem("10"));
+    $("#11.description").val(localStorage.getItem("11"));
+    $("#12.description").val(localStorage.getItem("12"));
+    $("#13.description").val(localStorage.getItem("13"));
+    $("#14.description").val(localStorage.getItem("14"));
+    $("#15.description").val(localStorage.getItem("15"));
+    $("#16.description").val(localStorage.getItem("16"));
+    $("#17.description").val(localStorage.getItem("17"));
+    
+    timeTracker();
+})
 
+// clear all stored values in calendar/local storage
+// this needs to be looped, doesn't seem right to be just like this
 $("#btn-clear").on("click", function () {
     window.localStorage.removeItem(["8"]);
     window.localStorage.removeItem(["9"]);
