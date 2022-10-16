5dfc87bd62a4ae7a709f8806c0469921faa11f55
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-16 00:49:14 -0400

    improvements to all code and additional items

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,33 @@ var startHour = 8;
 var endHour = 17;
 
 $(document).ready(function () {
-    // saveBtn add a listener method 
+    // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // get values and save into varialbles
+        // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
     
-        // Save time as key and text as value in local storage
+        // Save text in local storage
         localStorage.setItem(time, text);
-        })
+    })
     
+    $(".deleteBtn").on("click", function () {
+        // Get nearby values of the time key
+        var deleteTime = $(this).parent().attr("id");
+    
+        //remove single item from localstorage and refresh page to show deleted item gone
+        localStorage.removeItem(deleteTime);
+        window.location.reload();
+    })    
+
     function timeTracker() {
         //get current number of hours.
         var timeNow = moment().hour();
-        
+    
         // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id"));
-        
+    
             // To check the time and add the classes for background indicators
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
@@ -40,34 +49,18 @@ $(document).ready(function () {
                 $(this).removeClass("past");
                 $(this).addClass("future");
             }
-        })
+            })
+        }
+    
+    for (let i = startHour; i< endHour+1; i++) {
+        $("#" + [i] + " .description").val(localStorage.getItem([i]));
     }
-    
-    // store values on click
-    // need to figure out how to loop this process, below works but not good code
-        //psuedo-code
-        // for each # 8-17 set the value in storage to that same number
-        // ie start at 8: 8 set value in local storage key 8 to text value in 8 description
-    $("#8.description").val(localStorage.getItem("8"));
-    $("#9.description").val(localStorage.getItem("9"));
-    $("#10.description").val(localStorage.getItem("10"));
-    $("#11.description").val(localStorage.getItem("11"));
-    $("#12.description").val(localStorage.getItem("12"));
-    $("#13.description").val(localStorage.getItem("13"));
-    $("#14.description").val(localStorage.getItem("14"));
-    $("#15.description").val(localStorage.getItem("15"));
-    $("#16.description").val(localStorage.getItem("16"));
-    $("#17.description").val(localStorage.getItem("17"));
-    
+
     timeTracker();
 })
 
 // clear all stored values in calendar/local storage
-        // ?? is there a clear option, be sure to google that too, maybe thats easier, then no loop?
-        // testing looping
 $("#btn-clear").on("click", function () {
-    for (let i = startHour; i< endHour+1; i++) {
-        window.localStorage.removeItem([i]);
-        window.location.reload();
-    }
+    window.localStorage.clear();
+    window.location.reload();
 })
